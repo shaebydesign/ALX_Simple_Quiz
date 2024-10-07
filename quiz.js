6fa4b670aee158587a2e11c94e0d44fb6e764043
Shalom Metiboba <Shalommetiboba@gmail.com>
2024-10-07 19:51:12 +0100

    initial commit

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+    const correctAnswer = 4
+    return correctAnswer;
+}
+
+const quiz = document.querySelector("checked");
+
+var userAnswer = value;
+if (userAnswer === correctAnswer){
+    print("Correct! Well done.")
+} else{
+    print("That's incorrect. Try again!")
+}
+
+
+const submitAnswer = document.getElementById("submit-answer");
+submitAnswer.addEventListener('click')
